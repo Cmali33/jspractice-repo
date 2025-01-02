e0dfdba1d1928fd56e3971ce4fb708e944202099
cmali33 <malicanchal@gmail.com>
2025-01-02 14:31:48 +0530

    Added new file

---
+++ Navfiles/App.jsx
@@ -0,0 +1,17 @@
+import Nav from "./Nav";
+import About from "./About";
+import Home from './Home'
+import Work from "./Mywork";
+import Contact from "./Contact";
+import Footer from "./Footer";
+function App() {
+  return <>
+    <Nav></Nav>
+    <Home></Home>
+    <About></About>
+    <Work></Work>
+    <Contact></Contact>
+    <Footer></Footer>
+  </>
+}
+export default App;
